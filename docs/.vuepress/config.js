fe02db781df8aea7640bbd2174680f3d7f808349
dilipchandima <dilipchandima@gmail.com>
2023-01-01 18:42:35 +0530

    guide

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "Guide",
+        link: "/guide/",
+      },
+      {
+        text: "Config",
+        link: "/config/",
+      },
+      {
+        text: "VuePress",
+        link: "https://v1.vuepress.vuejs.org",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          title: "Guide",
+          collapsable: false,
+          children: ["", "using-vue"],
+        },
+      ],
+    },
+  },
+};
